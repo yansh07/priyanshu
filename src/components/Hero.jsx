fea83e406695306ca41bda82f39483e892dcd8ec
pksingh <pksingh69313@gmail.com>
2025-08-09 17:25:21 +0530

    Fixed: Responsiveness and text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,20 @@
 export const Hero = () => {
   return (
-    <div className="container max-w-5xl grid-cols-1 py-4 mx-auto lg:flex lg:flex-row md:py-20 lg:py-32 xl:py-32 lg:max-w-8xl">
-      
-      {/* Left Column */}
-      <div className="m-12 lg:m-5 md:ml-20 md:px-28">
-        <p className="mb-4 text-xl font-soft text-white/80 xl:text-2xl">
-          Hi, I am Priyanshu
-        </p>
-        <h1 className="inline-block text-5xl font-semibold leading-tight text-white lg:text-6xl font-soft md:text-6xl xl:text-7xl">
+    <header className="px-12 py-12 text-left md:py-32 md:ml-20 md:px-24">
+      <p className="mb-4 text-white/80 font-soft text-md md:text-lg">
+        Hi, I am Priyanshu
+      </p>
+
+      <div className="flex flex-col space-y-8 lg:flex-row lg:items-center lg:space-y-0 lg:space-x-8 md:gap-4">
+        <h1 className="text-5xl font-bold leading-none text-white font-soft md:text-6xl text-pretty">
           Software Developer
         </h1>
-      </div>
-
-      {/* Right Column */}
-      <div>
-        <p className="m-12 text-xl leading-relaxed lg:ml-0 lg:text-xl font-soft text-white/80 max-w-prose xl:text-2xl md:ml-20 md:px-28 md:mt-3 md:text-2xl">
-          Just another BCA 2nd Year kid, Turning coffee into code, chaos into clarity.
-Building stuff with MERN & Python.
+        <p className="text-xl text-white/80 font-soft md:text-2xl max-w-prose">
+          Just another BCA 2nd Year kid, Turning coffee into code, chaos into
+          clarity. Building stuff with MERN & Python.
         </p>
       </div>
-
-    </div>
+    </header>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-tl from-[#0f0f0f] via-[#1c1c1c] to-[#2b2b2b]">
+    <div className="min-h-screen bg-[#1e1a1a]">
       <Navbar />
       <Hero />
     </div>
