5872e6ed9997aa3f1d2f118d89ad8bf2ff8d3a4c
pksingh <pksingh69313@gmail.com>
2025-08-11 19:12:19 +0530

    Added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,47 @@
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div>
+      <div class="relative mt-16 mb-4 h-[2px] w-full flex justify-center">
+        <div class="absolute max-w-4xl h-[2px]"></div>
+        <div class="absolute h-[2px] bg-white/20 backdrop-blur-xl rounded-full w-[300px] md:w-[450px] lg:w-[700px] xl:w-[1125px]"></div>
+      </div>
+      <div className="px-10 md:px-64 lg:px-80 lg:ml-12 xl:px-96 xl:ml-44">
+        <div className="flex gap-3 py-6 mt-auto">
+          <a
+            href="https://github.com/yansh07"
+            className="relative z-10 p-2 transition duration-200 backdrop-xl md:p-2 lg:p-3 hover:ease-in-out hover:scale-95"
+            aria-label="GitHub"
+          >
+            <i className="text-4xl md:text-2xl lg:text-4xl text-white/80 hover:text-white fa-brands fa-github"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/yansh08/"
+            className="relative z-10 p-2 transition duration-200 backdrop-xl md:p-2 lg:p-3 hover:ease-in-out hover:scale-95"
+            aria-label="LinkedIn"
+          >
+            <i className="text-4xl md:text-2xl lg:text-4xl hover:text-white text-white/80 fab fa-linkedin"></i>
+          </a>
+          <a
+            href="https://x.com/yansh_08"
+            className="relative z-10 p-2 transition duration-200 backdrop-xl md:p-2 lg:p-3 hover:ease-in-out hover:scale-95"
+            aria-label="Twitter"
+          >
+            <i
+              className="text-4xl md:text-2xl lg:text-4xl hover:text-white text-white/80 fa-brands fa-twitter"
+              aria-hidden="true"
+            ></i>
+          </a>
+          <a
+            href="/resume.pdf"
+            className="relative z-10 items-center p-3 text-lg font-medium transition duration-200 bg-transparent lg:text-2xl lg:p-4 text-white/70 hover:text-white font-soft md:p-2 md:text-lg hover:ease-in-out hover:scale-95"
+            aria-label="Resume"
+          >
+            Resume
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
