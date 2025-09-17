70c958b165fc10429b159f0739632b1608bd5ac5
pksingh <pksingh69313@gmail.com>
2025-09-17 22:03:44 +0530

    Added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
-    <div className='sticky top-0'>
-        <nav className='border max-w-screen flex flex-wrap rounded-2xl p-2 bg-white/20 backdrop-blur-3xl'>
-            <img src="/pk.jpg" alt="priyanshu" className='w-12 h-12 rounded-lg'/>
-            <span className='font-[satoshi] text-gray-200 font-medium text-4xl tracking-tight px-2'>Priyanshu</span>
-            <a href="/resume.pdf" className='pt-2'>
-              <span className='font-[satoshi] text-gray-300 font-normal text-xl border-1 rounded-4xl  px-3 py-2 ml-8 bg-gradient-to-l from-[#adad03] via-[#362424] to-[#043492] brightness-125'>Resume</span>
-            </a>
-        </nav>
+    <div className="sticky top-0 md:absolute md:pt-6 md:px-6 lg:pt-12 lg:px-12 xl:px-12 xl:pt-12">
+      <nav className="border max-w-screen flex flex-wrap rounded-2xl p-2 bg-white/15 hover:shadow-blue-500 shadow backdrop-blur-3xl md:max-w-sm">
+        <img src="/pk.jpg" alt="priyanshu" className="w-12 h-12 rounded-lg" />
+        <span className="font-[satoshi] text-gray-200 font-semibold text-2xl tracking-tight px-2 pt-1">
+          Priyanshu
+        </span>
+        <a href="/resume.pdf" className="pt-2" target="_blank">
+          <span
+            className="font-[satoshi] text-gray-300 font-normal text-xl border border-gray-500 rounded-4xl px-5 py-2 ml-16 
+  bg-gradient-to-l from-[#adad03] via-[#362424] to-[#043492] brightness-125
+  transition-all duration-500 ease-in-out
+  hover:scale-110 hover:shadow-[0_0_15px_rgba(255,255,255,0.8)] hover:brightness-150 hover:bg-gradient-to-r hover:from-[#043492] hover:via-[#362424] hover:to-[#adad03]"
+          >
+            Resume
+          </span>
+        </a>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
