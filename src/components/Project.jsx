b58bbbce2b1dcec3e5ae4b8eb725accce659df55
pksingh <pksingh69313@gmail.com>
2025-09-18 13:34:59 +0530

    added project section

---
+++ src/components/Project.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+function Project() {
+  return (
+    <div>
+      <section className="grid grid-cols-1 p-4">
+        <h1
+          className="font-mono bg-gradient-to-r from-blue-50 to-blue-700 
+                  bg-clip-text text-transparent text-4xl px-4 font-bold mt-8 mb-4"
+        >
+          Projects
+        </h1>
+        <div
+          className="border-1 border-white/20  hover:border-cyan-400
+hover:rotate-3 hover:scale-105 hover:shadow-2xl
+ backdrop-blur-xl bg-black/20 p-4 rounded-xl hover:transition-all hover:duration-500 hover:-translate-y-2
+
+"
+        >
+          <img
+            src="/icon.svg"
+            alt="portfolio website"
+            className="w-12 h-12 rounded-lg mb-3"
+          />
+          <h3 className="font-mono text-gray-200 mb-2 font-bold text-2xl">
+            Notes App: PlanIt
+          </h3>
+          <p className="font-mono text-gray-400/80">
+            Engineered a full-stack application with JWT authentication and
+            OAuth/third-party authentication. Implemented image upload
+            functionality using Multer and Cloudinary.
+          </p>{" "}
+          <br />
+          <div className="font-mono italic text-gray-400 animate-gradient">
+            {/* MongoDB, Express.JS, React.JS, Node.JS, TailwindCSS, CI/CD */}
+            <ul className="font-mono text-gray-300 flex flex-wrap gap-3">
+                <li className="backdrop-blur-2xl bg-white/15 hover:text-white rounded-4xl p-1 text-sm">MongoDB</li>
+                <li className="backdrop-blur-2xl bg-white/15 hover:text-white rounded-4xl p-1 text-sm">Express.js</li>
+                <li className="backdrop-blur-2xl bg-white/15 hover:text-white rounded-4xl p-1 text-sm">React.js</li>
+                <li className="backdrop-blur-2xl bg-white/15 hover:text-white rounded-4xl p-1 text-sm">Node.js</li>
+                <li className="backdrop-blur-2xl bg-white/15 hover:text-white rounded-4xl p-1 text-sm">TailwindCSS</li>
+            </ul>
+          </div>
+          <div className="flex flex-row gap-12 justify-center mt-4">
+            <a href="https://github.com/yansh07/todo-frontend"><i class=" fa-solid text-lg font-mono fa-arrow-up-right-from-square text-gray-200"></i>
+            <span className="text-gray-50 font-mono px-2">Live</span>
+            </a>
+            <a href="https://planitfirst.vercel.app"><i class=" fa-brands fa-github text-lg font-mono text-gray-200"></i>
+            <span className="text-gray-50 font-mono px-2">GitHub</span>
+            </a>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Project;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
+import Project from '../components/Project';
 
 function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-900">
       <Navbar />
       <Hero />
+      <Project />
       
       {/* Custom Animations CSS */}
       <style jsx>{`
