9870b0285fbc170218d63ff351a81da123ef0d66
pksingh <pksingh69313@gmail.com>
2025-10-03 23:38:50 +0530

    update: added click spark component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,26 @@ import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import Project from "@/components/Projects";
 import SkillsMatrix from "@/components/Skills";
+import ClickSpark from "@/components/ClickSpark";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-900 overflow-x-hidden w-full">
-      <Navbar />
-      <Hero />
-      <Project />
-      <SkillsMatrix />
-      <BlogSection />
-      <Footer />
+      <ClickSpark
+        sparkColor="#fff"
+        sparkSize={10}
+        sparkRadius={15}
+        sparkCount={8}
+        duration={400}
+      >
+        {/* Your content here */}
+        <Navbar />
+        <Hero />
+        <Project />
+        <SkillsMatrix />
+        <BlogSection />
+        <Footer />
+      </ClickSpark>
     </div>
   );
 }
