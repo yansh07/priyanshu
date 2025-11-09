4d9e34aa39bcbd0d03d04c93ac669bf6de57bc63
pksingh <pksingh69313@gmail.com>
2025-11-09 16:40:34 +0530

    update: enjoy the new look

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
 import About from "@/components/About";
 import Skills from "@/components/Skills"; 
 import Projects from "@/components/Projects";
